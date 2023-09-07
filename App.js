621f1ea91531e47397db9651b2bd2f1535298b3d
manoahLinks <manoahluka@gmail.com>
2023-09-07 23:47:21 +0100

    intial commit

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Modal} from 'react-native';
+import WebView from 'react-native-webview';
 
 export default function App() {
+
+  const [visible, setVisible] = useState(false)
+
+  const openLink = () => {
+      setVisible(true)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <TouchableOpacity onPress={openLink}> 
+        <Text>Github</Text>
+      </TouchableOpacity>
+
+      <Modal
+        visible={visible}
+        animationType='slide'
+        presentationStyle='pageSheet'
+        onRequestClose={()=>{setVisible(false)}}
+      >
+        <WebView source={{ uri: 'https://github.com' }} />
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
