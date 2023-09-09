f22e8aacaf6a8bab8f1d2b57a10c261db130fba2
manoahLinks <manoahluka@gmail.com>
2023-09-09 17:33:34 +0100

    more changes tp the app page

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Modal} from 'react-native';
+import { StyleSheet, Image, Text, View, TouchableOpacity, Modal} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
+
 import WebView from 'react-native-webview';
 
 export default function App() {
@@ -13,9 +15,21 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      
-      <TouchableOpacity onPress={openLink}> 
-        <Text>Github</Text>
+
+      <Image
+        source={require('./assets/profile.jpeg')}
+        style={{width: '50%', height: '30%', borderRadius: 100}}
+      />
+
+      <View style={styles.detailsContainer}>
+        <Text style={styles.name}>Manoah Luka</Text>
+        <Text>Mano.dev</Text>
+        <Text>Manoahluka@gmail.com</Text>
+      </View>
+
+      <TouchableOpacity style={styles.githubButton} onPress={openLink}> 
+        <Icon name='github' size={30} color={`blue`} />
+        <Text style={styles.githubButtonText}>Github</Text>
       </TouchableOpacity>
 
       <Modal
@@ -24,7 +38,7 @@ export default function App() {
         presentationStyle='pageSheet'
         onRequestClose={()=>{setVisible(false)}}
       >
-        <WebView source={{ uri: 'https://github.com' }} />
+        <WebView source={{ uri: 'https://github.com/manoahLinks' }} />
       </Modal>
       <StatusBar style="auto" />
     </View>
@@ -34,8 +48,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#111111',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 30,
   },
+
+  githubButton:{
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+    borderColor: 'blue',
+    borderWidth: 1,
+    color: 'blue',
+    borderRadius: 5,
+  },
+
+  githubButtonText: {
+    color: 'blue'
+  },
+
+  name: {
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#fff'
+  },
+
+  detailsContainer:{
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    width: '80%',
+    backgroundColor: '#2F2F2F'
+  }
 });
